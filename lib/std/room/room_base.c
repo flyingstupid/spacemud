c7fb51c1eb32ed54717ce5dda445eb7c1e829373
Raymond <ray.lukes@gmail.com>
2018-03-31 10:44:12 -0700

    adding some functionality to the room base

--- lib/std/room/room_base.c
+++ lib/std/room/room_base.c
@@ -13,6 +13,28 @@ string query_desc()
 	return desc;
 }
 
+void set_desc(string _desc)
+{
+	desc = _desc;
+}
+
+void set_name(string name)
+{
+	name = name;
+}
+
+int add_exit(string exit, string path)
+{
+	int ret = 0;
+	
+	if(member_array(exit, keys(exits)))
+	{	
+		ret = 1;
+	}
+
+	exits[exit] = ({path});
+}
+
 string render_room()
 {
 	string out = "";
